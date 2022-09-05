3cd483c2d449d5d9e58eca240bb6dd0db258888a
amar-fr <46576837+amar-fr@users.noreply.github.com>
2022-09-05 22:34:01 +0200

    Add files via upload
    my Index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+
+
+
+<!DOCTYPE html>
+<!--[if IE]><![endif]-->
+<!--[if IE 8 ]> <html lang="fr-FR" prefix="og: http://ogp.me/ns#" class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]> <html lang="fr-FR" prefix="og: http://ogp.me/ns#" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="fr-FR" prefix="og: http://ogp.me/ns#" class="no-ie no-js"> <!--<![endif]-->
+
+<head>
+        <meta charset="UTF-8">
+        <title>AB-AUTO Enlevements voitures</title>
+        <!-- Respecter la largeur du mobile -->
+	    <meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	    <!--  -->
+	    <meta name="description" content="Enlèvement d&#039;épaves gratuit, rachat de voitures occasions, épaviste gratuit en région parisienne, Ile de France, Département Paris, Département Essone 91, Département Haut de seine 92, Département Seine saint denis 93, Département Val-de-Marne 94, Département Val-d'Oise 95, Département Hauts-de-Seine 77, Département Yvellines 78, IDF, Seine-Maritime 76,  Eure 27,  Oise 60" />
+		<meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+		<link rel="canonical" href="http://enlevements-voitures-gratuit.fr/" />
+		<meta property="og:locale" content="fr_FR" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="enlèvement épaves gratuit IDF - enleve-epave.go.yo.fr" />
+		<meta property="og:description" content="Enlèvement d&#039;épaves gratuit, Achat de véhicules occasions, Achat de voitures occasions, Dépannage, Ile de France, Département Paris, Département Essone 91, Département Haut de seine 92, Département Seine saint denis 93, Département Val-de-Marne 94, Département Val-d'Oise 95, Département Hauts-de-Seine 77, Département Yvellines 78, IDF, Seine-Maritime 76,  Eure 27,  Oise 60" />
+		<meta property="og:url" content="http://enlevements-voitures-gratuit.fr/" />
+		<meta property="og:site_name" content="enlevements-voitures-gratuit.fr/" />
+<style>
+#id {
+	display: table; 
+	vertical-align: middle; 
+	width: 100% !important; 
+  	text-align: center; 
+  	display: table; height: 600px; 
+}
+#h_great {
+	font-size : 110px;
+}
+
+</style>		
+</head>
+<body>
+  <div id="id">    
+    <div style="display: table-cell; vertical-align: middle;">
+	    <div>
+	      	<h1 style="color: red;">Bienvenue chez AB-auto *Corbeil*</h1>
+	    	<h2>Site en construction, il sera bientot en ligne</h2>
+	    	<h2>Merci de votre compr�hension</h2>
+	    	
+<?php
+	$i = 0;
+	$k = 1;
+	while ($i<6) {		
+		echo "<h".$k.">Bonjour http://TestSVN ==>h".$k."</h><br/>";
+		$i ++;
+		$k = $k + 1;
+	}
+
+?>
+	    </div>
+  	</div>
+  </div>
+</body>
+</html>
